e9e41cd882c65f87b6d4cb7d99c17059c576a7b3
Farhan Palilati <146071772+FarhanPalilat@users.noreply.github.com>
2025-07-26 21:39:22 +0800

    penambahan seeder pengaturan

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UserSeeder::class);
         $this->call(GuruSeeder::class);
         $this->call(SiswaSeeder::class);
+        $this->call(PengaturanSeeder::class);
     }
 }
